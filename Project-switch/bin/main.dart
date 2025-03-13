b6af275ffd9b4a7be4e512819d5f33139ebb2d73
Shivatmika654 <shivasivani2004@gmail.com>
2025-03-13 13:28:18 +0530

    Update main.dart

--- Project-switch/bin/main.dart
+++ Project-switch/bin/main.dart
@@ -1,1 +1,14 @@
+import 'package:lab4/switch1.dart';
+import 'package:lab4/switch2.dart';
+import 'dart:io';
+void main(List<String> arguments) {
+	switch1 sd1=switch1();
+	switch2 sd2=switch2();
+	//print("enter month");
+	print("enter fruit");
+	String x=stdin.readLineSync()!;
+	//sd1.demo1(x);
+	print(sd2.demo2(x));
+
+}
 
